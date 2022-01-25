f624127bee372568e9c1e780e72cc746f7d235d1
Amelia <95876231+Amelia-William@users.noreply.github.com>
2022-01-25 12:33:02 -0500

    Welcome Screen Update

--- Sketch.java
+++ Sketch.java
@@ -2,34 +2,57 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(600, 600);
+
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  float[] circleY = new float[40];
+
   public void setup() {
-    background(210, 255, 173);
+    background(132, 164, 240);
+    for (int i = 0; i < circleY.length; i++) {
+      circleY[i] = random(height);
+   }
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
+    
+    background(132, 164, 240);
     stroke(255);
-    line(50, 125, 70, 50);  
+    fill(255);
+    for (int i = 0; i < circleY.length; i++) {
+      float circleX = width * i / circleY.length;
+      ellipse(circleX, circleY[i], 15, 15);
+  
+      circleY[i]++;
+  
+      if (circleY[i] > height) {
+        circleY[i] = 0;
+      }
+    }
+
+    ellipse(300, 500, 800, 300);
+
+    fill(0);
+    textSize(20);
+    text("Welcome to Christmas Queries!", 160, 100);
+
+    textSize(20);
+    text("Choose a level to get started:", 170, 150);
+
+    stroke(197, 209, 237);
+    fill(255);
+    ellipse(200, 350, 100, 100);
+    ellipse(300, 350, 100, 100);
+    ellipse(400, 350, 100, 100);
+
+    fill(0);
+    textSize(20);
+    text("Easy", 175, 350);
+    text("Medium", 265, 350);
+    text("Hard", 375, 350);
+
   }
   
   // define other methods down here.
